e8e4f8d02cd207a6790816c3c721fe434042e5f5
nvcompsci <31483891+nvcompsci@users.noreply.github.com>
2019-11-07 08:45:45 -0500

    make one Gremlin, start array/matrix challenge

--- LifeSim.java
+++ LifeSim.java
@@ -16,13 +16,28 @@ import javax.swing.JPanel;
  * @author jword
  */
 public class LifeSim extends JPanel {
-
+    Gremlin[] population = new Gremlin[30];
+    Gremlin[][] spaces = new Gremlin[20][20];
+    
+    //Constructor for World, LifeSim
+    public LifeSim() {
+        //Use for loop to make 15
+        
+        Gremlin newGremlin = new Gremlin();
+        population[0] = newGremlin;
+        spaces[10][10] = newGremlin;
+        
+    }
+    
     @Override
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
         this.setBackground(Color.DARK_GRAY);
-        Gremlin grem1 = new Gremlin();
-        grem1.draw(g, 100, 100);
+        //Use for loop to draw all spaces
+        
+        if (spaces[0][0] == null) continue;
+        spaces[10][10].draw(g, 100, 100);
+        
     }    
     
    public static void main(String[] args) {
